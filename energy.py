cd7bf8c6676e634d4229a63d0cdac57b6fd3d73f
Thomas L Randall <tlranda@clemson.edu>
2019-12-06 15:01:36 -0500

    Simplify processing

--- energy.py
+++ energy.py
@@ -1,4 +1,4 @@
-import time, threading, subprocess, shlex, warnings, numpy as np
+import time, subprocess, shlex, numpy as np
 from datetime import datetime, timedelta
 from copy import deepcopy as dcpy
 
@@ -30,63 +30,62 @@ def measure_energy(duration, runtimes=500):
     runtime = 0.0
     joules = 0.0
     for i in range(runtimes):
+      original_joules = dcpy(joules)
       print("Iteration {0:03d} -- begin".format(i))
-      start = time.time()
+      profileStart = time.time()
       energyProfile(duration)
-      finish = time.time()
-      print("Iteration {0:03d} -- end ({1} seconds)".format(i, finish-start))
-      runtime += (finish - start)
-      # Performs same job as np.genfromtxt but with better tolerance to incomplete data
-      nvidia_smi_data = []
-      with open("nvidia.log", "r") as log:
-        for x in log.readlines():
-          entry = [datum.lstrip().rstrip() for datum in x.split(',')]
-          # Last entry likely to be incomplete, but all other entries should have length of 3
-          if len(entry) != 3:
-            break
-          # Format: [DATETIME, GPU_ID, WATTS]
-          nvidia_smi_data.append([datetime.strptime(entry[0], "%Y/%m/%d %H:%M:%S.%f"),
-                                  int(entry[1]),
-                                  float(entry[2])])
-      '''
-      # Ignore the numpy incomplete records warning (we're aware it will happen)
-        # genfromtxt is tolerant to incomplete records, which often occur since we likely kill nvidia_smi mid-write
-      with warnings.catch_warnings():
-        warnings.filterwarnings('ignore', r'Some errors were detected !')
-        # Numpy function reads the csv and returns numpy array of the data
-        '' UNKNOWN ERROR (needs debugging/replication aid):
-              line 2219, in genfromtxt
-              output = np.array(data, dtype)
-              ValueError: could not convert string to float: '???'
-        ''
-        nvidia_smi_data = np.genfromtxt("nvidia.log", delimiter=",", dtype=str,\
-                                        autostrip=True, invalid_raise=False,\
-                                        unpack=False,\
-                                        converters={\
-                                          0: lambda x: datetime.strptime(str(x)[2:-1], "%Y/%m/%d %H:%M:%S.%f"),\
-                                          1: lambda y: int(y),\
-                                          2: lambda z: float(z)})
-      '''
-      # Use datetime and timedelta objects to parse the nvidia_smi timestamps correctly
-      time_prev = None
-      for line in nvidia_smi_data:
+      profileEnd = time.time()
+      print("Iteration {0:03d} -- end ({1} seconds)".format(i, profileEnd - profileStart))
+      start = time.time()
+      # Process nvidia.log for energy data
         # Format: [DATETIME, GPU_ID, WATTS]
-        # Select only data from GPU 0 (we may need to make this an argument, but more efficient to tell nvidia-smi via its -i argument)
-        if line[1] != 0:
-          continue
-        if time_prev is not None:
-          temp_prev = dcpy(time_prev)
-          time_prev = line[0]
-          # timedelta can be converted to float by dividing by the desired time resolution
-          iteration_time = (time_prev - temp_prev) / timedelta(seconds=1)
-          watts = line[2]
-          # Joules = Watts * Time(seconds)
-          joules += watts * iteration_time
-        else:
-          time_prev = line[0]
+      time_sum = 0.0
+      with open("nvidia.log", "r") as log:
+        # Datetime/timedelta objects parse nvidia_smi timestamps
+        time_prev = None
+        for x in log.readlines():
+          # Sometimes nvidia_smi writes a whole bunch of \x00's
+          try:
+            entry = [datum.lstrip().rstrip() for datum in x.split(',')]
+            # Last entry likely to be incomplete, but all other entries should have length of 3
+            if len(entry) != 3:
+              break
+            # Format: [DATETIME, GPU_ID, WATTS]
+              # Select only data from GPU 0 (we may need to make this an argument, but more efficient to tell nvidia-smi via its -i argument)
+            if int(entry[1]) != 0:
+              continue
+            temp_prev = dcpy(time_prev)
+            time_prev = datetime.strptime(entry[0], "%Y/%m/%d %H:%M:%S.%f")
+            if temp_prev is not None:
+              # timedelta can be converted to float by dividing by the desired time resolution
+              iteration_time = (time_prev - temp_prev) / timedelta(seconds=1)
+              time_sum += iteration_time
+              watts = float(entry[2])
+              # Joules = Watts * Time(seconds)
+              joules += watts * iteration_time
+          except Exception:
+            # Discard the entry and move on with life
+            time_prev = temp_prev
+            continue
+      finish = time.time()
+      runtime += (finish - start)
+      print("Iteration {0:03d} -- processed in {1} seconds".format(i, finish-start))
+      print("\tIteration {0:03d} -- {1} joules (representing {2} seconds)".format(i, joules - original_joules, time_sum))
+      if joules - original_joules < 1.0:
+        raise ValueError("No joules detected??")
     return [joules/float(runtimes), runtime/float(runtimes)]
 
 if __name__ == "__main__":
-  result = measure_energy(3,5)
+  import sys
+  if len(sys.argv) == 1:
+    result = measure_energy(3,5)
+  elif len(sys.argv) == 2:
+    result = measure_energy(int(sys.argv[1]), 5)
+  elif len(sys.argv) == 3:
+    result = measure_energy(int(sys.argv[1]), int(sys.argv[2]))
+  else:
+    print("Unsure how to process that commandline")
+    print("USAGE: python3 energy.py <sleep_time=3> <runtimes=5>")
+    exit()
   print("Average joules = {0}, Average time = {1}".format(result[0], result[1]))
 
